16c8fad3882e8c47af36d93f1dc0bc86a7e9f9b2
Armin Golzar <armingolzar78@gmail.com>
2025-11-24 16:33:56 +0330

    half of the convolution layer was writen

--- src/train_from_scratch.py
+++ src/train_from_scratch.py
@@ -10,7 +10,7 @@ class MyDense(tf.keras.layers.Layer):
     def build(self, input_shape):
         
         in_features = int(input_shape[-1])
-        limit = tf.sqrt(6/ (in_features, self.units))
+        limit = tf.sqrt(6.0 / (in_features, self.units))
 
         self.w = self.add_weight(shape=(in_features, self.units), initializer=tf.random_uniform_initializer(-limit, limit), trainable=True)
 
@@ -26,3 +26,29 @@ class MyFlatten(tf.keras.layers.Layer):
         batch = tf.shape(inputs[0])
         return tf.reshape(inputs, (batch, -1))
     
+class MyConv2D(tf.keras.layers.Layer):
+
+    def __init__(self, filters, kernel_size, strides=(1,1), padding="same"):
+        super().__init__()
+        self.filters = filters
+        self.kernel_size = (kernel_size if isinstance(kernel_size, tuple) else (kernel_size, kernel_size))
+        self.strides = (strides if isinstance(strides, tuple) else (strides, strides))
+        self.padding = padding.upper()
+
+
+    def build(self, input_shape):
+        
+        kh, kw = self.kernel_size
+        in_channels = int(input_shape[-1])
+
+        limit = tf.sqrt(6.0 / (kh * kw * in_channels + self.filters))
+
+        self.kernel = self.add_weight(shape=(kh, kw, in_channels, self.filters), initializer=tf.random_uniform_initializer(-limit, limit), trainable=True)
+        self.bias = self.add_weight(shape=(self.filters,), initializer="zeros", trainable=True)
+
+    def call(self, inputs):
+
+        kh, kw = self.kernel_size
+        sh, sw = self.strides
+
+        
